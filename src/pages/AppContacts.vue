84a3358d0a15bcec2c7635eb8e964a2c4e441e40
francescofalcone05 <ciccio_97@live.it>
2024-07-23 15:57:43 +0200

    form in AppContacts.vue

--- src/pages/AppContacts.vue
+++ src/pages/AppContacts.vue
@@ -1,20 +1,97 @@
 <script>
+import axios from 'axios';
+import store from '../data/store.js';
 export default {
-    name: "AppHome"
+    name: "ContactForm",
+    data() {
+        return {
+            store,
+            name: '',
+            email: '',
+            message: '',
+            loading: false,
+            success: false,
+            errors: {}
+        }
+    },
+    methods: {
+        sendForm() {
+            this.loading = true;
+            const data = {
+                name: this.name,
+                email: this.email,
+                message: this.message
+            };
+
+            // pulisco l'array con i messaggi
+            this.errors = {};
+
+            // Importante - Stiamo comunicando con Laravel, quindi non è più obbligatorio inserire gli headers con il Content-Type
+            // come abbiamo fatto invece quando comunicavamo direttamente con gli script PHP
+            axios.post(`${store.localHostUrl}/api/contacts`, data).then((response) => {
+                this.success = response.data.success;
+                if (!this.success) {
+                    this.errors = response.data.errors;
+                } else {
+                    // ripulisco i campi di input
+                    this.name = '';
+                    this.email = '';
+                    this.message = '';
+                }
+                this.loading = false;
+            });
+        },
+    }
 }
 </script>
 
 <template>
-
-    <section>
-        <h1 class="mt-5 mb-3">Contacts</h1>
+    <section class="contact_me py-5">
+        <div class="inner-wrapper">
+            <div class="container-fluid text-center">
+                <h2 class="text-uppercase">contact me</h2>
+                <div v-if="success" class="alert alert-success text-start" role="alert">
+                    Messaggio inviato con successo!
+                </div>
+                <div class="row">
+                    <form @submit.prevent="sendForm()" class="col-12 text-start">
+                        <div class="mb-3">
+                            <input class="border-0 border-bottom form-control" :class="{ 'is-invalid': errors.name }"
+                                type="text" name="name" id="name" placeholder="Name" v-model="name">
+                            <p v-for="(error, index) in errors.name" :key="`message-error-${index}`"
+                                class="invalid-feedback">
+                                {{ error }}
+                            </p>
+                        </div>
+                        <div class="mb-3">
+                            <input class="border-0 border-bottom form-control" :class="{ 'is-invalid': errors.email }"
+                                type="text" name="email" id="email" placeholder="Email" v-model="email">
+                            <p v-for="(error, index) in errors.email" :key="`message-error-${index}`"
+                                class="invalid-feedback">
+                                {{ error }}
+                            </p>
+                        </div>
+                        <div class="mb-3">
+                            <textarea class="border-0 border-bottom form-control"
+                                :class="{ 'is-invalid': errors.message }" name="message" id="message" cols="30"
+                                rows="10" placeholder="Message" v-model="message"></textarea>
+                            <p v-for="(error, index) in errors.message" :key="`message-error-${index}`"
+                                class="invalid-feedback">
+                                {{ error }}
+                            </p>
+                        </div>
+                        <button class="btn btn-lg btn-primary text-white" type="submit" :disabled="loading">{{ loading ?
+                            'Sending...' : 'Send'
+                            }}</button>
+                    </form>
+                </div>
+            </div>
+        </div>
     </section>
-
 </template>
 
-
 <style scoped>
-h1 {
+section {
     color: white;
 }
 </style>
